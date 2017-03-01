c1d543b5d39d8e3dad7b52b6015bb4f9db7e144c
Berzeg07 <berezg86@gmail.com>
2017-03-01 16:45:44 +0300

    29.14

--- tech.php
+++ tech.php
@@ -1,19 +1,21 @@
 ﻿<?php require_once('includes/header.php'); ?>
 
 <br><br>		
-<a href="#" class="button-1">Заказать звонок</a>	
+<a href="#" class="button-1">Login</a>	
 <br><br>	
-<h1>Пример заголовка</h1>
+<h2>Пример заголовка</h2>
 <br><br>	
-<form action="#">
-	<input type="text" value="" placeholder="Инпут тайп текст">		
-	<input type="submit" value="Значение" placeholder="Инпут тайп сабмит">		
+<form class="dd-form" action="#">
+	
+		<input type="text" value="" placeholder="Инпут тайп текст">		
+		<input type="submit" class="button-1 button-1_submit" value="Login">	
+		
 </form>		
 
 
 <?php require_once('includes/footer.php'); ?>
 
-	
+
 </body>
 </html>
 
