75fc24ad7fe238217d5cab71b4c2f8b89e0502da
engine77 <zaur.1kager@gmail.com>
2017-03-02 11:17:57 +0300

    cabinet

--- cabinet.php
+++ cabinet.php
@@ -1,4 +1,74 @@
 <?php require_once('includes/header.php'); ?>
+<?php require_once('includes/header-top2.php'); ?>
+<div class="dfborder100"></div>	
+	<section class="section">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">					
+						<div class="dflogin">
+							<span>Login - </span>1EJ7FEgPngFnTv41x7zApEcMXgDk6UVhdq
+							<a href="#" class="dflogout">Logout</a>
+						</div>
+						<div class="dfbalance">
+							<div class="dfboxcab">
+								<span class="dfba">My balance:</span>
+								<span class="dfba2">18.256M</span>
+							</div>
+							<div class="dfboxcab">
+								<span class="dfba">Demo:</span>
+								<span class="dfba2 dfright">500</span>
+							</div>
+							<a href="#" class="dflinks1">Add more coins</a>
+							<a href="#" class="dflinks2">Payments</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>	
+		<div class="dfborder100"></div>		
+
+		<div class="clearfix"></div>
+
+		<section class="section">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">			
+						<div class="dftextblock">
+							<h4>Partner program</h4>
+							<p>We share the promotional code with your friends and get <br>
+							10% of their revenue. In addition, earn 100 coins for each invitation
+							  other (which activates your code and earn 100 coins). A friend who
+							  activates the code will get 100 coins to your account.</p>
+							
+							<span class="dfyourlink">Your link:</span>	
+							<input type="text" class="dfinp" value="www.btcbattle.com/djd#5/dfsdf">
+							<a href="#" class="dfbuttoncopy">Copy</a>
+
+							<span class="dffriend">Earned from friends:</span>
+							<span class="dfmycoins">1820 coins</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>	
+		<div class="dfborder100"></div>		
+		<div class="clearfix"></div>
+
+		<section class="section">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">		
+						<div class="dfgetcoin">
+							<span class="dfbonus">Daily bonus:</span>
+							<span class="df3coins">3 coins</span>
+							<a href="#">Get</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>	
+			
+
 
 <?php require_once('includes/footer.php'); ?>
 
---
+++ includes/header-top2.php
@@ -0,0 +1,26 @@
+<section class="section section_padding section_header">
+	<div class=" container header_container">
+		<div class="row">
+			<div class="col-md-4">
+				<div class="dd-logo">
+					<a href="#"><img src="img/logo.png" alt=""></a>
+				</div>
+			</div>
+			<div class="col-md-8">
+				<div class="box-wrap">
+					<div class="dd-box-header">
+						<span>1 = 100 satoshi</span>
+					</div>
+					<div class="dd-box-header">
+						<a class="button-1" href="#">18.256M</a>
+					</div>
+					<div class="dd-box-header">
+						<a class="button-1 button-1__padding" href="#">Cabinet</a>
+					</div>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+			<div class="clearfix"></div>
+		</div>
+	</div>
+</section>
