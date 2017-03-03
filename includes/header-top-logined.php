8c0ffd62d1af9f739d72fa6f6d563a146198046b
Berzeg07 <berezg86@gmail.com>
2017-03-03 10:13:24 +0300

    29.14

--- includes/header-top-logined.php
+++ includes/header-top-logined.php
@@ -1,12 +1,12 @@
 <section class="section section_padding section_header">
 	<div class=" container header_container">
 		<div class="row">
-			<div class="col-md-4">
+			<div class="col-md-4 col-sm-4">
 				<div class="dd-logo">
 					<a href="#"><img src="img/logo.png" alt=""></a>
 				</div>
 			</div>
-			<div class="col-md-8">
+			<div class="col-md-8 col-sm-8">
 				<div class="box-wrap">
 					<div class="dd-box-header_l dd-pt1">
 						<div class="dd-free m0auto">
